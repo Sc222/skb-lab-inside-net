731b85e7931a4205fec895e121cfa513c36081c0
Sc222 <v.a.shaternikov@gmail.com>
2021-05-13 19:42:58 +0500

    LoginActivity process onClick events

--- Android/app/src/main/java/com/sc222/insidenet/ui/login/LoginActivity.java
+++ Android/app/src/main/java/com/sc222/insidenet/ui/login/LoginActivity.java
@@ -2,15 +2,32 @@ package com.sc222.insidenet.ui.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.sc222.insidenet.R;
+import com.sc222.insidenet.databinding.ActivityLoginBinding;
+import com.sc222.insidenet.ui.proflie.ProfileActivity;
+import com.sc222.insidenet.ui.register.RegisterActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //TODO: create LoginViewModel, process input fields, make server requests
+
+        binding.signInButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, ProfileActivity.class);
+            startActivity(intent);
+        });
+
+        binding.signUpButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, RegisterActivity.class);
+            startActivity(intent);
+        });
     }
 }
