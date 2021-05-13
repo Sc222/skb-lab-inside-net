46e96d85d882c8605aed9c01e0b480a4b6fee89b
Sc222 <v.a.shaternikov@gmail.com>
2021-05-13 19:35:10 +0500

    add LoginActivity layout

--- Android/app/src/main/java/com/sc222/insidenet/ui/launch/LauncherActivity.java
+++ Android/app/src/main/java/com/sc222/insidenet/ui/launch/LauncherActivity.java
@@ -2,9 +2,10 @@ package com.sc222.insidenet.ui.launch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
 import com.sc222.insidenet.R;
+import com.sc222.insidenet.ui.login.LoginActivity;
 
 public class LauncherActivity extends AppCompatActivity {
 
@@ -12,5 +13,10 @@ public class LauncherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+
+        //TODO: start LoginActivity or ProfileActivity depending on user login status (stored in accountManager)
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        overridePendingTransition(0, 0);
     }
 }
