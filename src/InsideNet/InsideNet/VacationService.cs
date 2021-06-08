83b22200cef7b898cae40a7a1ff4338788cb9950
malyshev.v <malyshev.v@skbkontur.com>
2021-06-08 19:44:42 +0500

    vacation service

---
+++ src/InsideNet/InsideNet/VacationService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Storage;
+using Storage.Entities;
+
+namespace InsideNet.Services
+{
+    public class VacationService
+    {
+        private readonly IRepository<Vacation> vacations;
+
+        public VacationService(IRepository<Vacation> vacations)
+        {
+            this.vacations = vacations;
+        }
+
+        public Vacation[] GetPersonVacations(Guid personId)
+        {
+            return vacations.Find(v => v.PersonId == personId);
+        }
+
+        public Vacation[] GetForPeriod(DateTime from, DateTime to)
+        {
+            return vacations.Find(v => v.From.Date >= from.Date && v.To.Date <= to.Date);
+        }
+
+        public void Create(Vacation vacation)
+        {
+            vacations.Create(vacation);
+        }
+
+        public void Update(Vacation vacation)
+        {
+            vacations.Update(vacation);
+        }
+
+        public void Delete(Guid id)
+        {
+            vacations.Delete(id);
+        }
+    }
+}
