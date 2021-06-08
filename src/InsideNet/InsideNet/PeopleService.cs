72eeb9e8ce3f5d9035a6b5f003af7d3a8fdee8f0
malyshev.v <malyshev.v@skbkontur.com>
2021-06-08 16:01:41 +0500

    person access rights to separate service

--- src/InsideNet/InsideNet/PeopleService.cs
+++ src/InsideNet/InsideNet/PeopleService.cs
@@ -33,28 +33,7 @@ namespace InsideNet.Services
 
         public void Update(Person person)
         {
-            var dbAccessRights = accessRights.Find(r => r.PersonId == person.Id);
-
-            var (newRights, approvedRights, removedRights) = GetChangedRights(person.AccessRights, dbAccessRights);
-
             people.Update(person);
-
-            accessRights.CreateRange(newRights);
-            accessRights.UpdateRange(approvedRights);
-            accessRights.DeleteRange(removedRights);
-        }
-
-        private static (PersonAccessRights[] newRights, PersonAccessRights[] approvedRights, PersonAccessRights[] removedRights) GetChangedRights(
-            PersonAccessRights[] personAccessRights,
-            PersonAccessRights[] dbAccessRights)
-        {
-            var ids = personAccessRights.Select(r => r.AccesRightId);
-            var dbIds = dbAccessRights.Select(r => r.AccesRightId);
-            var newRights = personAccessRights.Where(r => !dbIds.Contains(r.AccesRightId)).ToArray();
-            var oldRights = personAccessRights.Where(r => dbIds.Contains(r.AccesRightId)).ToArray();
-            var approvedRights = oldRights.Where(r => r.IsApproved).ToArray();
-            var removedRights = dbAccessRights.Where(r => !ids.Contains(r.AccesRightId)).ToArray();
-            return (newRights, approvedRights, removedRights);
         }
 
         public Person[] SearchByName(string searchedName)
---
+++ src/InsideNet/InsideNet/PersonAccessRightsService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Storage;
+using Storage.Entities;
+
+namespace InsideNet.Services
+{
+    public class PersonAccessRightsService
+    {
+        private readonly IRepository<PersonAccessRights> personAccessRights;
+
+        public PersonAccessRightsService(IRepository<PersonAccessRights> personAccessRights)
+        {
+            this.personAccessRights = personAccessRights;
+        }
+
+        public void RequestAccess(Guid personId, params Guid[] rightsIds)
+        {
+            var accessRights = ConvertToPersonAccessRights(personId, rightsIds);
+            personAccessRights.CreateRange(accessRights);
+        }
+
+        public void ApproveAccess(Guid personId, params Guid[] rightsIds)
+        {
+            var accessRights = ConvertToPersonAccessRights(personId, rightsIds, true);
+            personAccessRights.UpdateRange(accessRights);
+        }
+
+        public void RemoveAccess(Guid personId, params Guid[] rightsIds)
+        {
+            var accessRights = ConvertToPersonAccessRights(personId, rightsIds);
+            personAccessRights.DeleteRange(accessRights);
+        }
+
+        private static IEnumerable<PersonAccessRights> ConvertToPersonAccessRights(Guid personId, Guid[] rightsIds, bool isApproved = false)
+        {
+            var accessRights = rightsIds.Select(id => new PersonAccessRights
+            {
+                AccesRightId = id,
+                PersonId = personId,
+                IsApproved = isApproved
+            });
+            return accessRights;
+        }
+    }
+}
