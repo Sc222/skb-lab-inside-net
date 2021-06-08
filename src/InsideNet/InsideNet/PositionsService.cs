d8492791c87bbc8f6b925694fdc85f311a7403c6
malyshev.v <malyshev.v@skbkontur.com>
2021-06-08 19:49:16 +0500

    positions service

---
+++ src/InsideNet/InsideNet/PositionsService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Storage;
+using Storage.Entities;
+
+namespace InsideNet.Services
+{
+    public class PositionsService
+    {
+        private readonly IRepository<Position> positions;
+
+        public PositionsService(IRepository<Position> positions)
+        {
+            this.positions = positions;
+        }
+
+        public Position[] GetAll()
+        {
+            return positions.GetAll();
+        }
+
+        public void Create(Position position)
+        {
+            positions.Create(position);
+        }
+
+        public void Update(Position position)
+        {
+            positions.Update(position);
+        }
+
+        public void Delete(Guid id)
+        {
+            positions.Delete(id);
+        }
+    }
+}
