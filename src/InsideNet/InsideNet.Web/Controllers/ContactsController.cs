e060ca1cbd1bfb791ef949a5126b369398598000
malyshev.v <malyshev.v@skbkontur.com>
2021-06-10 11:25:29 +0500

    contacts control

---
+++ src/InsideNet/InsideNet.Web/Controllers/ContactsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using AutoMapper;
+using InsideNet.Services;
+using InsideNet.Web.Auth;
+using InsideNet.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsideNet.Web.Controllers
+{
+    [ApiController]
+    [Authentication]
+    [Route("api/[controller]/{personId}")]
+    public class ContactsController : ControllerBase
+    {
+        private readonly ContactsService contactsService;
+        private readonly IMapper mapper;
+
+        public ContactsController(ContactsService contactsService, IMapper mapper)
+        {
+            this.contactsService = contactsService;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public PersonModel[] GetPersonContacts(Guid personId)
+        {
+            var contacts = contactsService.GetPersonContacts(personId);
+            return mapper.Map<PersonModel[]>(contacts);
+        }
+
+        [AccessFor("", true)]
+        [HttpPut("add/{contactId}")]
+        public void AddToContacts(Guid personId, Guid contactId)
+        {
+            contactsService.AddToContacts(personId, contactId);
+        }
+
+        [AccessFor("", true)]
+        [HttpDelete("remove/{contactId}")]
+        public void RemoveFromContacts(Guid personId, Guid contactId)
+        {
+            contactsService.RemoveFromContacts(personId, contactId);
+        }
+    }
+}
