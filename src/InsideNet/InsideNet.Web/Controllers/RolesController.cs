160b123649cca6c2e7af6aab4de8b34e3a8fbaa6
malyshev.v <malyshev.v@skbkontur.com>
2021-06-10 12:08:32 +0500

    roles controller

---
+++ src/InsideNet/InsideNet.Web/Controllers/RolesController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using AutoMapper;
+using InsideNet.Services;
+using InsideNet.Web.Auth;
+using InsideNet.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Storage.Entities;
+
+namespace InsideNet.Web.Controllers
+{
+    [ApiController]
+    [Authentication]
+    [Route("api/[controller]")]
+    public class RolesController : ControllerBase
+    {
+        private readonly RolesService rolesService;
+        private readonly IMapper mapper;
+
+        public RolesController(IMapper mapper, RolesService rolesService)
+        {
+            this.mapper = mapper;
+            this.rolesService = rolesService;
+        }
+
+        [AccessFor("canViewRoles")]
+        [HttpGet("{id}")]
+        public RoleModel Get(Guid id)
+        {
+            return mapper.Map<RoleModel>(rolesService.Get(id));
+        }
+
+        [AccessFor("canViewRoles")]
+        [HttpGet("all")]
+        public RoleModel[] GetAll()
+        {
+            return mapper.Map<RoleModel[]>(rolesService.GetAll());
+        }
+
+        [AccessFor("canEditRoles")]
+        [HttpPost("create")]
+        public RoleModel Create([FromBody] RoleModel accessRight)
+        {
+            var entity = mapper.Map<Role>(accessRight);
+            return mapper.Map<RoleModel>(rolesService.Create(entity));
+        }
+
+        [AccessFor("canEditRoles")]
+        [HttpPost("update")]
+        public RoleModel Update([FromBody] RoleModel accessRight)
+        {
+            var entity = mapper.Map<Role>(accessRight);
+            return mapper.Map<RoleModel>(rolesService.Update(entity));
+        }
+
+        [AccessFor("canEditRoles")]
+        [HttpPost("delete")]
+        public void Delete([FromBody] RoleModel accessRight)
+        {
+            rolesService.Delete(accessRight.Id);
+        }
+    }
+}
