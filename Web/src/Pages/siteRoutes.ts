97c59472d681fbb9054c3b133f15230adb835f30
Sc222 <v.a.shaternikov@gmail.com>
2022-01-08 01:52:44 +0500

    Add site routes enum model

---
+++ Web/src/Pages/siteRoutes.ts
@@ -0,0 +1,16 @@
+export enum SiteRoutes {
+    //root routes
+    home = "/",
+    login = "/login",
+    register = "/register",
+    search = "/search",
+    user = "/user",
+    userId = ":userId",
+
+    //user nested routes
+    profile = "/profile",
+    editProfile = "/edit-profile",
+    manageAccess = "/manage-access",
+    timeOff = "/time-off",
+    //all new plugin links go here
+}
