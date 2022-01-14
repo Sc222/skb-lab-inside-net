844fba5a478c17239373e7676e81ea281487f25f
Sc222 <v.a.shaternikov@gmail.com>
2022-01-14 23:55:51 +0500

    New profile page design

--- Android/app/src/main/java/com/sc222/insidenet/ui/mainSections/fragments/profile/ProfileFragment.java
+++ Android/app/src/main/java/com/sc222/insidenet/ui/mainSections/fragments/profile/ProfileFragment.java
@@ -4,12 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
