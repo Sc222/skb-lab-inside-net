8a1d05df8c59bcbee38ca9863aef2a89740e1fec
Vlad <malyshev.v@skbkontur.ru>
2022-03-09 12:35:51 +0500

    update base filler

--- src/InsideNet/Tests/BaseFiller.cs
+++ src/InsideNet/Tests/BaseFiller.cs
@@ -29,28 +29,31 @@ namespace Tests
             var contactsRepo = new Repository<PersonContact>(contextFactory);
             var peopleRepo = new Repository<Person>(contextFactory);
             var peopleRolesRepo = new Repository<PersonRole>(contextFactory);
+            var departmentsRepo = new Repository<Department>(contextFactory);
 
             var roles = GenerateRoles();
             var positions = GenerateUserPositions();
-            var people = GeneratePeople(positions, roles);
+            var departments = GenerateDepartments();
+            var people = GeneratePeople(positions, departments);
 
             rolesRepo.CreateRange(roles.Select(r => r.Value));
             positionsRepo.CreateRange(positions.Select(p => p.Value));
+            departmentsRepo.CreateRange(departments.Select(d => d.Value));
             peopleRepo.CreateRange(people.Select(p => p.Value));
 
             var vacation = new Vacation
             {
                 From = DateTime.Now,
                 To = DateTime.Now + TimeSpan.FromDays(14),
-                PersonId = people[UserType.SimpleDimple].Id
+                PersonId = people[UserType.Programmer].Id
             };
 
             vacationsRepo.Create(vacation);
 
             var contact = new PersonContact
             {
-                PersonId = people[UserType.Hr].Id,
-                ContactId = people[UserType.SimpleDimple].Id
+                PersonId = people[UserType.DepartmentManager].Id,
+                ContactId = people[UserType.Programmer].Id
             };
 
             contactsRepo.Create(contact);
@@ -62,61 +65,85 @@ namespace Tests
 
         private enum UserType
         {
-            Hr,
-            SimpleDimple,
+            DepartmentManager,
+            Programmer,
             Admin
         }
 
 
         private Dictionary<UserType, Role> GenerateRoles()
         {
-            var hr = new Role
+            var departmentManager = new Role
             {
-                AllowedActions = new List<string> {"canEdit"}, //заглушка, пока не придумал экшены
-                Name = "Hr"
+                AllowedActions = new List<string>
+                {
+                    "canEditUsers",
+                    "canEditVacations",
+                    "canEditPositions"
+                }, 
+                Name = "DepartmentManager"
             };
-            var popIt = new Role
+            var programmer = new Role
             {
-                AllowedActions = new List<string> { "canEdit" }, //заглушка, пока не придумал экшены
-                Name = "SimpleDimple"
+                AllowedActions = new List<string>(), 
+                Name = "Regular"
             };
             var admin = new Role
             {
-                AllowedActions = new List<string> { "canEdit" }, //заглушка, пока не придумал экшены
+                AllowedActions = new List<string> 
+                { 
+                    "canEditUsers",
+                    "canEditRoles",
+                    "canSetRoles",
+                    "canViewRoles",
+                    "canResolveAccessRequests",
+                    "canEditVacations",
+                    "canEditPositions",
+                    "canEditNotificationsChannels"
+                },
                 Name = "Admin"
             };
             return new Dictionary<UserType, Role>
             {
-                {UserType.Hr, hr},
-                {UserType.SimpleDimple, popIt},
+                {UserType.DepartmentManager, departmentManager},
+                {UserType.Programmer, programmer},
                 {UserType.Admin, admin}
             };
         }
 
-        private Dictionary<UserType, Position> GenerateUserPositions() => new Dictionary<UserType, Position>
+        private Dictionary<UserType, Department> GenerateDepartments() => new Dictionary<UserType, Department>
         {
-            {UserType.Hr, new Position {Name = "Hr"}},
-            {UserType.SimpleDimple, new Position {Name = "Programmer"}},
-            {UserType.Admin, new Position {Name = "RUSS1488"}}
+            { UserType.DepartmentManager, new Department { Name = "HR-отдел" } },
+            { UserType.Programmer, new Department { Name = "Отдел программистов" } },
+            { UserType.Admin, new Department { Name = "Главное отделение" } }
         };
 
-        private Dictionary<UserType, Person> GeneratePeople(Dictionary<UserType, Position> positions, Dictionary<UserType, Role> roles)
+        private Dictionary<UserType, Position> GenerateUserPositions() => new Dictionary<UserType, Position>
         {
-            var hr = new Person
+            {UserType.DepartmentManager, new Position {Name = "Менеджер"}},
+            {UserType.Programmer, new Position {Name = "Программист"}},
+            {UserType.Admin, new Position {Name = "Админ"}}
+        };
+
+        private Dictionary<UserType, Person> GeneratePeople(Dictionary<UserType, Position> positions, Dictionary<UserType, Department> departments)
+        {
+            var manager = new Person
             {
                 Email = "m@m.com",
-                Login = "hr",
-                Password = "hr",
+                Login = "manager",
+                Password = "manager",
                 FullName = "Екатерина Андреевна Зас",
-                Position = positions[UserType.Hr]
+                Position = positions[UserType.DepartmentManager],
+                Department = departments[UserType.DepartmentManager]
             };
-            var simpleDimple = new Person
+            var programmer = new Person
             {
                 Email = "k@k.com",
                 Login = "user",
                 Password = "user",
                 FullName = "Анатолий Генадьевич Зас",
-                Position = positions[UserType.SimpleDimple],
+                Position = positions[UserType.Programmer],
+                Department = departments[UserType.Programmer],
                 Telegram = "@abc"
             };
             var admin = new Person
@@ -124,24 +151,25 @@ namespace Tests
                 Email = "a@a.com",
                 Login = "admin",
                 Password = "admin",
-                FullName = "Супер Лютый Админ",
+                FullName = "Евгений Кожемяко",
                 Position = positions[UserType.Admin],
+                Department = departments[UserType.Admin],
                 PhoneNumber = "88005553535"
             };
             return new Dictionary<UserType, Person>
             {
-                {UserType.Hr, hr},
-                {UserType.SimpleDimple, simpleDimple},
+                {UserType.DepartmentManager, manager},
+                {UserType.Programmer, programmer},
                 {UserType.Admin, admin}
             };
         }
 
-        private List<PersonRole> GeneratePersonRoles(Dictionary<UserType, Person> people, Dictionary<UserType, Role> rights) =>
+        private List<PersonRole> GeneratePersonRoles(Dictionary<UserType, Person> people, Dictionary<UserType, Role> roles) =>
             people
                 .Select(kvp => new PersonRole
                 {
                     PersonId = kvp.Value.Id,
-                    RoleId = rights[kvp.Key].Id
+                    RoleId = roles[kvp.Key].Id
                 })
                 .ToList();
     }
