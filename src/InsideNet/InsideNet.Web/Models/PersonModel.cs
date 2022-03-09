84605b3f0d3f37c32262e2fbf5ba77ae92d12cd4
Vlad <malyshev.v@skbkontur.ru>
2022-03-09 10:07:50 +0500

    add avatarUrl to person

--- src/InsideNet/InsideNet.Web/Models/PersonModel.cs
+++ src/InsideNet/InsideNet.Web/Models/PersonModel.cs
@@ -23,5 +23,7 @@ namespace InsideNet.Web.Models
         public string Email { get; set; }
 
         public string PhoneNumber { get; set; }
+
+        public string AvatarUrl { get; set; }
     }
 }
--- src/InsideNet/Storage/Entities/Person.cs
+++ src/InsideNet/Storage/Entities/Person.cs
@@ -21,5 +21,7 @@
         public string Email { get; set; }
 
         public string PhoneNumber { get; set; }
+
+        public string AvatarUrl { get; set; }
     }
 }
