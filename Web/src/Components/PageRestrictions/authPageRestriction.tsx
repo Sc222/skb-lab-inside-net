5dead1959e4ca756fe08afa8865bc02e6ab2e6d4
Sc222 <v.a.shaternikov@gmail.com>
2022-03-12 00:58:26 +0500

    Update browser history when redirecting to login page

--- Web/src/Components/PageRestrictions/authPageRestriction.tsx
+++ Web/src/Components/PageRestrictions/authPageRestriction.tsx
@@ -39,7 +39,9 @@ export const AuthPageRestriction: FunctionComponent<AuthPageRestriction> = ({ ac
     // trying to go to when they were redirected. This allows us to send them
     // along to that page after they login, which is a nicer user experience
     // than dropping them off on the home page.
-    return <Navigate to={SiteRoute.login} state={{ from: location }} replace />;
+    // FIXME:  from: location does not always work actually
+    // !!! replace is `false` to detect this redirection in android web app
+    return <Navigate to={SiteRoute.login} state={{ from: location }} replace={false} />;
   }
 
   if (!authProfileScope) {
